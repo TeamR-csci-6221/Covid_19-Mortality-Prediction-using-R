d350c211b0da08b91b04a5d7efa23baa75498bcc
ERshyama <shyama.kumari.arunachalam@gmail.com>
2024-04-12 17:28:23 -0400

    Changed FE Templates Added Pearson Correlation Heatmap Extended Country comparator to new features

--- main.R
+++ main.R
@@ -3,6 +3,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(leaflet)
+library(jsonlite)
 library(rnaturalearth)
 library(rnaturalearthdata)
 
@@ -12,33 +13,48 @@ dw <- read.csv("/Users/dog/Documents/csci_6221/owid-covid-data.csv")
 dw$date <- as.Date(dw$date)
 
 unique_locations <- unique(dw$location)
-metrics <- c("New Cases", "New Deaths", "Total Cases", "Total Deaths", "Vaccinated Per Hundred", "Fully Vaccinated Per Hundred")
+metrics <- c("New Cases", "New Deaths", "Total Cases", "Total Deaths", "Vaccinated Per Hundred", "Fully Vaccinated Per Hundred", "Weekly Hospital_Admission Per Million", "Weekly ICU_Admission Per Million", "Positive Rate", "Total Test", "New Test", "Total Test Per Thousand","New Test Per Thousand")
 
 ui <- fluidPage(
-  titlePanel("COVID-19 Country-wide Analysis"),
-  sidebarLayout(
-    sidebarPanel(
-      uiOutput("location_ui"), # Dynamic UI for locations
-      selectInput("metric", "Select Metric", choices = metrics, selected = "New Cases"),
-      dateInput("start_date", "Start Date:", value = "2021-01-01"),
-      dateInput("end_date", "End Date:", value = "2021-12-31"),
-      actionButton("add_location", "Add Another Country", icon = icon("plus")),
-      actionButton("submit", "Submit")
-    ),
-    mainPanel(plotOutput("cases_plot"),
-              leafletOutput("world_map") # Add this line for the map output
-    )
+  tags$head(
+    tags$style(HTML("
+      .navbar { background-color: #b2d8d8 ; } /* Light Turquoise navbar */
+      .navbar-nav .nav-item .nav-link { color: #ffffff; } /* White navbar text */
+      body { background-color: #e9ecef;} /* Light gray background */
+      .shiny-output-error { background-color: #FFF; color: #000; } /* Error message styling */
+      .shiny-output-error:before { content: 'Error: '; }
+      /* Additional padding for the sidebar to align with the body content */
+      .well { padding-top: 40px; }
+    "))
+  ),
+  navbarPage("COVID-19 Analysis App",
+             tabPanel("Country comparator tool",
+                      sidebarLayout(
+                        sidebarPanel(
+                          uiOutput("location_ui"), # Dynamic UI for locations
+                          actionButton("add_location", "Add Another Country", icon = icon("plus")),
+                          selectInput("metric", "Select Metric", choices = metrics, selected = "New Cases"),
+                          dateInput("start_date", "Start Date:", value = "2021-01-01"),
+                          dateInput("end_date", "End Date:", value = "2021-12-31"),
+                          actionButton("submit", "Submit")
+                        ),
+                        mainPanel(
+                          plotOutput("cases_plot"),
+                          leafletOutput("world_map") # Add this line for the map output
+                        )
+                      )
+             ),
+             tabPanel("Pearsons Correlation Heatmap",
+                      mainPanel(
+                        plotOutput("heatmap_plot") # Heatmap output here, without sidebar
+                      )
+             )
   )
 )
-
 
 server <- function(input, output) {
 
   locations <- reactiveValues(list = list("United States")) # Start with 1 default location
-
-  # country_to_iso <- unique(dw[, c("location", "iso_code")])
-  # names(country_to_iso) <- country_to_iso$location
-  # country_to_iso <- country_to_iso$iso_code
 
   # Assuming dw is your dataset which includes columns 'location' for country names and 'ISO' for their ISO codes
   # Create a mapping from country names to ISO codes
@@ -127,6 +143,20 @@ server <- function(input, output) {
       y_metric <- "people_vaccinated_per_hundred"
     }else if (metric == "Fully Vaccinated Per Hundred") {
       y_metric <- "people_fully_vaccinated_per_hundred"
+    }else if (metric == "Weekly Hospital_Admission Per Million") {
+      y_metric <- "weekly_hosp_admissions_per_million"
+    }else if (metric == "Weekly ICU_Admission Per Million") {
+      y_metric <- "weekly_icu_admissions_per_million"
+    }else if (metric == "Positive Rate") {
+      y_metric <- "positive_rate"
+    }else if (metric == "Total Test") {
+      y_metric <- "total_tests"
+    }else if (metric == "New Test") {
+      y_metric <- "new_tests"
+    }else if (metric == "Total Test Per Thousand") {
+      y_metric <- "total_tests_per_thousand"
+    }else if (metric == "New Test Per Thousand") {
+      y_metric <- "new_tests_per_thousand"
     }
 
 
@@ -149,6 +179,34 @@ server <- function(input, output) {
         location_data <- location_data %>%
           filter(!is.na(new_cases) & new_cases != 0)
       }
+      if (metric == "Weekly Hospital_Admission Per Million") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "Weekly ICU_Admission Per Million") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "Positive Rate") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "Total Test") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "New Test") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "Total Test Per Thousand") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
+      if (metric == "New Test Per Thousand") {
+        location_data <- location_data %>%
+          filter(!is.na(new_cases) & new_cases != 0)
+      }
       filtered_data <- rbind(filtered_data, location_data)
     }
 
@@ -159,7 +217,14 @@ server <- function(input, output) {
         labs(x = "Date", y = sprintf("Number of %s", metric),
              title = paste(metric, "in Selected Countries from", start_date, "to", end_date),
              color = "Country") +
-        theme_minimal()
+        theme_minimal() +
+        theme(axis.text.x = element_text(size = 12, face = "bold", angle = 45, hjust = 1),  # Bold and larger x-axis text with angle
+              axis.text.y = element_text(size = 12, face = "bold"),  # Bold and larger y-axis text
+              axis.title.x = element_text(size = 14, face = "bold"),  # Bold and larger x-axis title
+              axis.title.y = element_text(size = 14, face = "bold"),  # Bold and larger y-axis title
+              plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  # Bold and larger plot title, centered
+              legend.title = element_text(size = 12, face = "bold"),  # Bold and larger legend title
+              legend.text = element_text(size = 12, face = "bold"))  # Bold and larger legend text
     })
 
     output$world_map <- renderLeaflet({
@@ -188,7 +253,48 @@ server <- function(input, output) {
     })
 
   })
+
+  output$heatmap_plot <- renderPlot({
+    # Filter data for the specified date
+    date_filtered_data <- dw %>% filter(date == as.Date("2023-01-01"))
+
+    # Select the columns of interest for rows and columns in the heatmap
+    row_variables <- date_filtered_data %>%
+      select(total_deaths_per_million, total_cases_per_million,
+             weekly_icu_admissions_per_million, weekly_hosp_admissions_per_million)
+
+    col_variables <- date_filtered_data %>%
+      select(people_fully_vaccinated_per_hundred, people_vaccinated_per_hundred,
+             total_boosters_per_hundred, population_density,
+             median_age)
+
+    # Compute Pearson's correlation
+    correlation_matrix <- cor(row_variables, col_variables, use = "pairwise.complete.obs", method = "pearson")
+
+    # Convert the correlation matrix to a format suitable for ggplot2
+    correlation_data <- as.data.frame(as.table(correlation_matrix))
+
+    names(correlation_data) <- c("RowVariable", "ColVariable", "Correlation")
+
+    # Generate the heatmap
+    ggplot(correlation_data, aes(x = RowVariable, y = ColVariable, fill = Correlation)) +
+      geom_tile() +
+      # scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = 0) +
+      scale_fill_gradient2(low = "skyblue", high = "darkred", mid = "white", midpoint = 0) +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      labs(title = "Pearson's Correlation Heatmap on 01/01/2023", x = "", y = "") +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "bold"),  # Bold and larger x-axis text
+            axis.text.y = element_text(size = 12, face = "bold"),  # Bold and larger y-axis text
+            axis.title.x = element_text(size = 14, face = "bold"),  # Bold and larger x-axis title
+            axis.title.y = element_text(size = 14, face = "bold"),  # Bold and larger y-axis title
+            plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  # Bold and larger plot title, centered
+            legend.title = element_text(size = 12, face = "bold"),  # Bold and larger legend title
+            legend.text = element_text(size = 12, face = "bold"))  # Bold and larger legend text
+  })
+
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
